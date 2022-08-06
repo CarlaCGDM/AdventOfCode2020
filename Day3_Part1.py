35280ff63cd2637f3fa03b37fb844987a81ab086
CarlaCGDM <92323990+CarlaCGDM@users.noreply.github.com>
2022-08-06 20:36:01 +0100

    Update Day3_Part1.py

--- Day3_Part1.py
+++ Day3_Part1.py
@@ -16,18 +16,18 @@ rows = puzzle_input.splitlines()
 for r in rows:
     tree_map.append(list(r))
     
-width = len(tree_map[0])-1
+width = len(tree_map[0])
 
 #if index is too big we subtract the width to loop over
 
 col = 3
 trees = 0
 
-for row in tree_map:
-    if col > width:
-        col -= width
+for row in tree_map[1:]:
     if row[col] == '#':
         trees += 1
     col += 3
+    if col >= width:
+        col -= width
 
 print(trees)
