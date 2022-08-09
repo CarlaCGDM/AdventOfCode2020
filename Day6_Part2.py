12d3331e5c9c8b4fc908306e0f9c578dbda91613
CarlaCGDM <92323990+CarlaCGDM@users.noreply.github.com>
2022-08-09 13:47:26 +0100

    Update Day6_Part2.py

--- Day6_Part2.py
+++ Day6_Part2.py
@@ -14,20 +14,27 @@ a
 
 b"""
 
+
 #Answers by the same person are in the same line
 #Answers by different groups are separated by empty lines
 
 def count_repeated_chars(group_answers):
     chars = list(group_answers[0])
+    remove = []
     for line in group_answers:
         for char in chars:
             if char not in line:
-                chars.remove(char)
+                remove.append(char)
+                
+    for char in remove:
+        if char in chars:
+            chars.remove(char)
+        
     return len(chars)
     
 groups = []
-
 group = []
+
 for line in puzzle_input.splitlines():
     if line != "":
         group.append(line)
@@ -35,8 +42,6 @@ for line in puzzle_input.splitlines():
         groups.append(group)
         group = []
 groups.append(group)
-
-print(groups)
     
 result = 0
 
