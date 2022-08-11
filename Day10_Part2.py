8ebd29072aceeeabba49fb3424464a23ff6faf93
CarlaCGDM <92323990+CarlaCGDM@users.noreply.github.com>
2022-08-11 10:59:41 +0100

    Update Day10_Part2.py

--- Day10_Part2.py
+++ Day10_Part2.py
@@ -15,38 +15,14 @@ adapters = [int(x) for x in adapters]
 
 my_adapter = max(adapters) + 3
 adapters.append(my_adapter)
-adapters.append(0)
 
 adapters.sort()
 
-#figure out which adapters can connect to the current joltage
+#count adapters
 
-def find_connections(current_adapter,adapters):
-    connections = []
-    for adapter in adapters:
-        if (adapter - current_adapter) in range(1,4):
-            connections.append(adapter)
-    return connections
-  
-#build graph
-
-connections = {}
+tally = {0 : 1}
 
 for adapter in adapters:
-    connections[adapter] = find_connections(adapter,adapters)
-    
- #find paths
+    tally[adapter] = tally.get(adapter-3,0) + tally.get(adapter-2,0) + tally.get(adapter-1,0)
 
-def find_all_paths(graph,start,end,path=[]):
-    path = path + [start]
-    if start == end:
-        return [path]
-    paths = []
-    for node in graph[start]:
-        new_paths = find_all_paths(graph, node, end, path)
-        for new_path in new_paths:
-            paths.append(new_path)
-    return paths
-        
-print(len(find_all_paths(connections,0,my_adapter)))
-    
+print(tally[adapters[-1]])
